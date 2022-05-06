8f621f5c1a322e2a639fb22ade5870c1bf21f4f3
Hans Erik Jepsen <haeje@jfmedier.dk>
2022-05-06 13:59:12 +0200

    Implemented commander damage component

---
+++ src/components/CommanderDamage.svelte
@@ -0,0 +1,34 @@
+<style>
+    .counter-wrapper {
+        display: flex;
+        flex-flow: column;
+        align-items: center;
+    }
+</style>
+
+<script lang="ts">
+    export let increaseLife: () => void;
+    export let decreaseLife: () => void;
+
+    let damage = 0;
+
+    const increase = () => {
+        if (damage < 21) {
+            damage++;
+            decreaseLife()
+        }
+    }
+
+    const decrease = () => {
+       if (damage > 0) {
+           damage--;
+           increaseLife()
+        }
+    }
+</script>
+
+<div class="counter-wrapper">
+    <button class="inc" on:click={increase}> + </button>
+    <p>{damage}</p>
+    <button class="dec" on:click={decrease}> - </button>
+</div>
