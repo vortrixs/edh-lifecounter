b578c98ec4a860876bfd4c1310311dbd20f7c025
Hans Erik Jepsen <haeje@jfmedier.dk>
2022-05-06 13:58:53 +0200

    Implemented life counter component

---
+++ src/components/LifeCounter.svelte
@@ -0,0 +1,19 @@
+<style>
+    .life-wrapper {
+        display: flex;
+        gap: 10px;
+    }
+</style>
+
+<script lang="ts">
+    export let life: number;
+    export let increase: () => void;
+    export let decrease: () => void;
+    export let showModal: () => void;
+</script>
+
+<div class="life-wrapper">
+    <button on:click={decrease}> - </button>
+    <p on:click={showModal}>{life}</p>
+    <button on:click={increase}> + </button>
+</div>
