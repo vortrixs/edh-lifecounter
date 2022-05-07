36bb94808f861b0617f8c9932c1bbb6cef0c1d37
Hans Erik Jepsen <haeje@jfmedier.dk>
2022-05-07 08:58:32 +0200

    Added life ticker

--- src/components/LifeCounter.svelte
+++ src/components/LifeCounter.svelte
@@ -7,6 +7,30 @@
         align-items: center;
     }
 
+    .counters {
+        display: flex;
+        flex-flow: column;
+        align-items: center;
+    }
+
+    .ticker {
+        position: fixed;
+        font-size: 20px;
+        font-weight: 700;
+    }
+
+    .ticker.positive {
+        color: #46d846;
+    }
+
+    .ticker.negative {
+        color: #d84646;
+    }
+
+    .ticker.hidden {
+        display: none;
+    }
+
     .life {
         font-size: 80px;
     }
@@ -33,11 +57,40 @@
     export let decrease: () => void;
     export let showModal: () => void;
 
+    let timeout: NodeJS.Timeout;
+
+    const increaseLife = () => {
+        increase();
+        ticker++;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => ticker = 0, 2000)
+    }
+
+    const decreaseLife = () => {
+        decrease();
+        ticker--;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => ticker = 0, 2000)
+    }
+
+    let ticker = 0;
+
+    $: lifeTick = (ticker<=0?"":"+") + ticker;
     $: isDead = life <= 0;
+
+    $: tickerClasses = [
+        'ticker',
+        ticker === 0 ? 'hidden' : '',
+        ticker > 0 ? 'positive' : '',
+        ticker < 0 ? 'negative' : '',
+    ];
 </script>
 
 <div class="life-wrapper">
-    <button class="decrease" on:click={decrease}/>
-    <p class="life {isDead ? 'dead' : ''}" on:click={showModal}>{life}</p>
-    <button class="increase" on:click={increase}/>
+    <button class="decrease" on:click={decreaseLife}/>
+    <div class="counters">
+        <p class={tickerClasses.join(' ').trim()}>{lifeTick}</p>
+        <p class="life {isDead ? 'dead' : ''}" on:click={showModal}>{life}</p>
+    </div>
+    <button class="increase" on:click={increaseLife}/>
 </div>
